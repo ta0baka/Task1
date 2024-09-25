59438b0980eab36916e53e4d301b4b395c7cbdf0
anastasia <@ta0baka>
2024-09-25 13:01:36 +0300

    lab1

---
+++ app/src/main/java/com/example/laboratory1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.laboratory1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("surname", "Новикова");
+                startActivity(intent);
+            }
+        });
+    }
+}
